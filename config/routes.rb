88936cb380e5101ff1152724e3e258b0dd863e8c
harsha0609 <hv2595@gmail.com>
2018-03-21 10:43:46 +0530

    updated the app with forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'docs/index'
+
+  get 'docs/show'
+
+  get 'docs/new'
+
+  get 'docs/create'
+
+  get 'docs/update'
+
+  get 'docs/destroy'
+
   get 'maps/index'
 
   get 'maps/new'
@@ -9,9 +21,12 @@ Rails.application.routes.draw do
 
   get 'maps/onemore'
 
+  get 'ajax/:action', to: 'ajax#:action', :defaults => { :format => 'json' }
+
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
+  resources :pics
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320130108) do
+ActiveRecord::Schema.define(version: 20180321035810) do
+
+  create_table "docs", force: :cascade do |t|
+    t.string "title"
+    t.string "domain"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "fils", force: :cascade do |t|
+    t.string "title"
+    t.string "domain"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pics", force: :cascade do |t|
+    t.string "title"
+    t.string "domain"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
